aff3290ca007e5ff0b609f06521f214b515309ae
Curran <curran.kelleher@gmail.com>
2020-07-14 16:34:32 -0400

    Set up Rollup build

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+const buble = require('@rollup/plugin-buble');
+
+export default {
+  input: 'index.js',
+  external: ['d3'],
+  output: {
+    file: 'bundle.js',
+    format: 'iife',
+    sourcemap: true,
+    globals: { d3: 'd3' },
+  },
+  plugins: [buble()],
+};
