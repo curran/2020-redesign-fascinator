16f849a6d7069d3b15cbcfd7179db90dc6f4ce59
Curran <curran.kelleher@gmail.com>
2020-07-15 15:58:01 -0400

    Respond to resize

--- bundle.js
+++ bundle.js
@@ -1,14 +1,39 @@
 (function (d3) {
   'use strict';
 
-  const dataDir =  '.';
+  const dataDir =  `${window.location.origin}/wp-content/themes/stamen-2020/assets/data`
+    ;
 
   const fascinator = d3.select('.fascinator');
-  fascinator.style('background', 'red');
+
+  let state = {};
+  const setState = (newState) => {
+    state = { ...state, ...newState };
+    render();
+  };
+
+  const render = () => {
+    console.log(state);
+    const { data, width, height } = state;
+    fascinator.style('background', data ? 'green' : 'red');
+  };
+
+  const setDimensions = () => {
+    const div = fascinator.node();
+    console.log(div);
+    setState({
+      width: div.clientWidth,
+      height: div.clientHeight,
+    });
+  };
+
+  setDimensions();
+  window.addEventListener('resize', setDimensions);
 
   d3.json(dataDir + '/sampleWithTags.json').then((data) => {
-    fascinator.style('background', 'green');
-    console.log(data.map(d => d.acf['go-live_date']));
+    setState({
+      data: data.map((d) => d.acf['go-live_date']),
+    });
   });
 
 }(d3));
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 import { select, json } from 'd3';
 
 // Set this to true to build for production, false during development.
-const isProd = false;
+const isProd = true;
 
 const dataDir = isProd
   ? `${window.location.origin}/wp-content/themes/stamen-2020/assets/data`
   : '.';
 
 const fascinator = select('.fascinator');
-fascinator.style('background', 'red');
+
+let state = {};
+const setState = (newState) => {
+  state = { ...state, ...newState };
+  render();
+};
+
+const render = () => {
+  console.log(state);
+  const { data, width, height } = state;
+  fascinator.style('background', data ? 'green' : 'red');
+};
+
+const setDimensions = () => {
+  const div = fascinator.node();
+  console.log(div);
+  setState({
+    width: div.clientWidth,
+    height: div.clientHeight,
+  });
+};
+
+setDimensions();
+window.addEventListener('resize', setDimensions);
 
 json(dataDir + '/sampleWithTags.json').then((data) => {
-  fascinator.style('background', 'green');
-  console.log(data.map(d => d.acf['go-live_date']));
+  setState({
+    data: data.map((d) => d.acf['go-live_date']),
+  });
 });
