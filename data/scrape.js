e762a6184821fbfa2d519d0f8163ac4943b207d2
Curran <curran.kelleher@gmail.com>
2020-07-23 17:37:15 -0400

    Fetch listing

--- data/scrape.js
+++ data/scrape.js
@@ -1,4 +1,15 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
 
-console.log(fetch);
+const wordpressListingURL = 'https://stamenstaging.wpengine.com/wp-json/viz/v1/post';
+
+const fetchWordpressListing = async () => {
+  const response = await fetch(wordpressListingURL);
+  console.log(await response.json());
+};
+
+const main = async () => {
+  const wordpressListing = await fetchWordpressListing();
+};
+
+main();
